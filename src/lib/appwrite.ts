fa1500a4b49d561689d85297cd9419f255ed0e99
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-30 23:44:01 -0300

    First commit

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Databases } from 'appwrite';
+
+export const client = new Client();
+
+export const databases = new Databases(client)
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_HOST)
+  .setProject(import.meta.env.VITE_APPWRITE_KEY);
+
+
